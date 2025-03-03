ebc5d68abcfdbcad3c2e36487aec30d95ab803f8
sbx_user1051 <sbx_user1051@169.254.54.35>
2025-03-03 15:43:57 +0000

    Mon Mar 03 15:43:57 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,6 +8,7 @@ def checkout(skus: str) -> int:
     B_counter = 0
     C_counter = 0
     D_counter = 0
+    E_counter = 0
     total = 0
     if set(skus).issubset({"A", "B", "C", "D"}):
         for i in skus:
@@ -19,12 +20,20 @@ def checkout(skus: str) -> int:
                 C_counter += 1
             if i == 'D':
                 D_counter += 1
+            if i == 'E':
+                E_counter += 1
+        if (A_counter // 5) >= 1:
+            total += (A_counter // 5) * 200 + A_counter % 3 * values["A"]
+        else:
+            total += (A_counter // 3) * 130 + A_counter % 3 * values["A"]
 
-        total += (A_counter // 3) * 130 + A_counter % 3 * values["A"]
 
         total+= (B_counter // 2) * 45 + B_counter % 2 * values["B"]
 
         total += C_counter * values["C"] + D_counter * values["D"]
+
+        if E_counter >= 2:
+            total -= (E_counter // 2) * values["B"]
         return total
     else:
         return -1
@@ -35,4 +44,3 @@ def test_checkout():
     assert checkout('EEB') == 80
 if __name__ == '__main__':
     test_checkout()
-
