62d22dd62dc71540d0936431f9f191815a06eb80
sbx_user1051 <sbx_user1051@169.254.54.35>
2025-03-03 17:22:42 +0000

    Mon Mar 03 17:22:42 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -33,7 +33,6 @@ def checkout(skus: str) -> int:
 
         total += C_counter * values["C"] + D_counter * values["D"]
         total += E_counter * values["E"]
-        print("E_counter",  E_counter)
         if (E_counter // 2)>=1 and B_counter >= 1:
             #
             if (B_counter - (E_counter // 2) >= 0):
@@ -50,6 +49,7 @@ def test_checkout():
     assert checkout('EEB') == 80
     assert checkout('ABCDE') == 155
     assert checkout('AAABCDE') == 235
+    print(checkout('AAABCDEE'))
     assert checkout('AAABCDEE') == 240
     assert checkout('AAAAAAAA') == 330
     assert checkout('AAAAAAAAA') == 380
