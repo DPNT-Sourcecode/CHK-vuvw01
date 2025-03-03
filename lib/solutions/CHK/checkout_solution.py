1517126631b9b2272b464d3e9f6e957f27ec6133
sbx_user1051 <sbx_user1051@169.254.54.35>
2025-03-03 14:07:26 +0000

    Mon Mar 03 14:07:26 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,27 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    raise NotImplementedError()
+def checkout(skus: str) -> int:
+    values = {"A": 50, "B": 30, "C": 20, "D": 15}
+    A_counter = 0
+    B_counter = 0
+    C_counter = 0
+    D_counter = 0
+    try:
+        for i in skus:
+            if i == 'A':
+                A_counter += 1
+            if i == 'B':
+                B_counter += 1
+            if i == 'C':
+                C_counter += 1
+            if i == 'D':
+                D_counter += 1
+        if A_counter == 3:
+            values["A"] = 130
+        if B_counter == 2:
+            values["B"] = 45
+        total = A_counter * values["A"] + B_counter * values["B"] + C_counter * values["C"] + D_counter * values["D"]
+    except:
+        return -1
+
