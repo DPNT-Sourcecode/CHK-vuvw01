810ed1777a0aa03e568223b549f70209abb10f0d
sbx_user1051 <sbx_user1051@169.254.54.35>
2025-03-03 23:17:02 +0000

    Mon Mar 03 23:17:02 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -37,8 +37,7 @@ def apply_discounts(discounts: dict, skus_count: dict) -> int:# this function sh
             print("rule[1]:", rule[1])
             # check if
             total += rule[1]
-            item_count = skus_count[item]
-            skus_count[item] = item_count - rule[0]
+            print("total", total)
     return total
 
 
@@ -71,5 +70,6 @@ def checkout(skus: str) -> int:
     return total
 
 print(checkout("EEB"))
+
 
 
