001c1f91de876f39e5d25dc571b0965189b0316b
sbx_user1051 <sbx_user1051@169.254.54.35>
2025-03-03 19:14:00 +0000

    Mon Mar 03 19:14:00 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,13 +4,7 @@ import unittest
 # skus = unicode string
 def checkout(skus: str) -> int:
     values = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10}
-    A_counter = 0
-    B_counter = 0
-    C_counter = 0
-    D_counter = 0
-    E_counter = 0
-    F_counter = 0
-    total = 0
+    A_counter =  B_counter = C_counter = D_counter = E_counter = F_counter = total = 0
     if not set(skus).issubset({"A", "B", "C", "D", "E", "F"}):
         return -1
     for i in skus:
@@ -37,16 +31,10 @@ def checkout(skus: str) -> int:
     A_counter %= 3
     total+= A_counter * values["A"]
     total += C_counter * values["C"] + D_counter * values["D"] + E_counter * values["E"]
-    # every 3 Fs in the basket only pay for 2
-
-    # every 3 values * pay 2 times
     total += (F_counter // 3) * (2 * values["F"])
     F_counter %= 3
-    # pay remaining Fs
     total += F_counter * values["F"]
 
     return total
 
-def test() -> int:
-    assert checkout("ABCDEEFFF") == 
 
