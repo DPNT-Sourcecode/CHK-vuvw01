47aacd8399da02e8e03ab0a4fbc358c3a80fc488
sbx_user1051 <sbx_user1051@169.254.54.35>
2025-03-03 23:59:00 +0000

    Mon Mar 03 23:59:00 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -45,7 +45,7 @@ def apply_discounts(skus_count: dict) -> int:
 def checkout(skus: str) -> int:
     values = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10, "G": 20, "H": 10, "I": 35, "J": 60, "K": 80, "L": 90, "M": 15, "N": 40, "O": 10, "P": 50, "Q": 30, "R": 50, "S": 30, "T": 20, "U": 40, "V": 50, "W": 20, "X": 90, "Y": 10, "Z": 50}
     A_counter =  B_counter = C_counter = D_counter = E_counter = F_counter = total = 0
-    if not set(skus).issubset({"A", "B", "C", "D", "E", "F"}):
+    if not set(skus).issubset({"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}):
         return -1
     skus_count = Counter(skus)
     apply_offers(skus_count)
@@ -53,4 +53,5 @@ def checkout(skus: str) -> int:
     for item, remaining_skus_number in skus_count.items():
         total+= values[item]*remaining_skus_number
     return total
+
 
