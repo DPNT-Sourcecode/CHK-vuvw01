261b1ae87d1a3e8341a2f5ccf1c0e583895cc31f
sbx_user1051 <sbx_user1051@169.254.54.35>
2025-03-03 15:28:56 +0000

    Mon Mar 03 15:28:56 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -19,27 +19,13 @@ def checkout(skus: str) -> int:
                 C_counter += 1
             if i == 'D':
                 D_counter += 1
-        if A_counter > 3:
-            total += (A_counter // 3) * 130 + A_counter % 3 * values["A"]
-            print("total:" , total)
-        if B_counter > 3:
-            total+= (B_counter // 2) * 45 + B_counter % 2 * values["B"]
-            print("total 1:", total)
+
+        total += (A_counter // 3) * 130 + A_counter % 3 * values["A"]
+
+        total+= (B_counter // 2) * 45 + B_counter % 2 * values["B"]
+
         total += C_counter * values["C"] + D_counter * values["D"]
-        print("total 3:", total)
         return total
     else:
         return -1
-
-print(checkout("AAABBBCD"))
-def test_checkout():
-    assert checkout("AAAAAAAA") == 360
-    assert checkout("BBBBBBAAAA") == 315
-    assert checkout("AAABBBCD") == 240
-    assert checkout("ABCD") == 115
-    assert checkout("8") == -1
-
-
-# if __name__ == '__main__':
-#     test_checkout()
 
