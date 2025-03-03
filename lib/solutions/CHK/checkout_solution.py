abcac5653802c8992c784263e8b88b36feadf03e
sbx_user1051 <sbx_user1051@169.254.54.35>
2025-03-03 20:29:02 +0000

    Mon Mar 03 20:29:02 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,9 +1,21 @@
 import unittest
-
+#
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str) -> int:
     values = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10, "G": 20, "H": 10, "I": 35, "J": 60, "K": 80, "L": 90, "M": 15, "N": 40, "O": 10, "P": 50, "Q": 30, "R": 50, "S": 30, "T": 20, "U": 40, "V": 50, "W": 20, "X": 90, "Y": 10, "Z": 50}
+    discounts = {"A": [(5, 200), (3, 130)],
+                 "B": [(2, 45)],
+                 "E": [(2, "B")],
+                 "F": [(2, "F")],
+                 "H": [(5, 45), (10, 80)],
+                 "K": [(2, 150)],
+                 "N": [(3, "M")],
+                 "P": [(5, 200)],
+                 "Q": [(3, 80)],
+                 "R": [(3, "Q")],
+                 "U": [(3, "U")],
+                 "V": [(2, 90), (3, 130)]}
     A_counter =  B_counter = C_counter = D_counter = E_counter = F_counter = total = 0
     if not set(skus).issubset({"A", "B", "C", "D", "E", "F"}):
         return -1
@@ -36,4 +48,5 @@ def checkout(skus: str) -> int:
     total += F_counter * values["F"]
 
     return total
+
 
