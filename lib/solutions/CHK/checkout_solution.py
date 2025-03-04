093b4bea3c338692918dc9800ef3f3c9b0e7523c
sbx_user1051 <sbx_user1051@169.254.54.35>
2025-03-04 00:51:48 +0000

    Tue Mar 04 00:51:48 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -44,7 +44,8 @@ def apply_discounts(skus_count: dict) -> int:
             skus_count[item] -= (skus_count[item] // amount_of_free_items) * amount_of_free_items
     return total
 
-
+def apply_any_three_discouts(skus_count: dict) -> int:
+    pass
 
 def checkout(skus: str) -> int:
     values = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10, "G": 20, "H": 10, "I": 35, "J": 60, "K": 80, "L": 90, "M": 15, "N": 40, "O": 10, "P": 50, "Q": 30, "R": 50, "S": 30, "T": 20, "U": 40, "V": 50, "W": 20, "X": 90, "Y": 10, "Z": 50}
@@ -60,9 +61,9 @@ def checkout(skus: str) -> int:
     return total
 
 
-
-
-
-
-
-
+assert checkout("ABCDEFGHIJKLMNOPQRSTUVWXYZ") == 965
+assert checkout("UUUU") == 120
+assert checkout("HHHHHHHHHHHHHHHHHHHH") == 160
+assert checkout("LGCKAQXFOSKZGIWHNRNDITVBUUEOZXPYAVFDEPTBMQLYJRSMJCWH") == 1880
+assert checkout("AAAAAPPPPPUUUUEEBRRRQAAAHHHHHHHHHHVVVBBNNNMFFFKKQQQVVHHHHH") == 1640
+assert checkout("PPPPQRUVPQRUVPQRUVSU")  == 740
